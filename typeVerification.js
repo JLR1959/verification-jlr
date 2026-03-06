fe704c9093f7e06289b799c434393b2f06a27959
JLR1959 <jlouis40@hotmail.com>
2026-03-06 08:33:57 -0500

    Add files via upload

--- typeVerification.js
+++ typeVerification.js
@@ -9,6 +9,7 @@ const groupeInterieur = document.getElementById("pieces-interieur");
 const groupeExterieur = document.getElementById("pieces-exterieur");
 
 const indicateur = document.getElementById("mode-verification-indicateur");
+const bandeau = document.getElementById("bandeau-mode-verification");
 
 const selectPieces = document.getElementById("type-piece");
 
@@ -17,16 +18,22 @@ champType.value = type;
 boutonInterieur.classList.remove("actif");
 boutonExterieur.classList.remove("actif");
 
+bandeau.classList.remove("bandeau-interieur");
+bandeau.classList.remove("bandeau-exterieur");
+
 if(type === "interieur"){
 
 boutonInterieur.classList.add("actif");
 
-groupeInterieur.style.display="block";
-groupeExterieur.style.display="none";
+groupeInterieur.style.display = "block";
+groupeExterieur.style.display = "none";
 
-indicateur.style.display="block";
-indicateur.className="mode-interieur";
-indicateur.innerText="Mode de vérification : INTÉRIEUR";
+indicateur.style.display = "block";
+indicateur.className = "mode-interieur";
+indicateur.innerText = "Mode de vérification : INTÉRIEUR";
+
+bandeau.classList.add("bandeau-interieur");
+bandeau.innerText = "🏠 Mode actif : Vérification INTÉRIEURE";
 
 }
 
@@ -34,27 +41,30 @@ if(type === "exterieur"){
 
 boutonExterieur.classList.add("actif");
 
-groupeInterieur.style.display="none";
-groupeExterieur.style.display="block";
+groupeInterieur.style.display = "none";
+groupeExterieur.style.display = "block";
 
-indicateur.style.display="block";
-indicateur.className="mode-exterieur";
-indicateur.innerText="Mode de vérification : EXTÉRIEUR";
+indicateur.style.display = "block";
+indicateur.className = "mode-exterieur";
+indicateur.innerText = "Mode de vérification : EXTÉRIEUR";
+
+bandeau.classList.add("bandeau-exterieur");
+bandeau.innerText = "🌳 Mode actif : Vérification EXTÉRIEURE";
 
 }
 
-selectPieces.value="";
+selectPieces.value = "";
 
 }
 
 
 
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function(){
 
 const groupeInterieur = document.getElementById("pieces-interieur");
 const groupeExterieur = document.getElementById("pieces-exterieur");
 
-groupeInterieur.style.display="none";
-groupeExterieur.style.display="none";
+groupeInterieur.style.display = "none";
+groupeExterieur.style.display = "none";
 
 });
